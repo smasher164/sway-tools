834f3fdfb319e28cbf097f1c2e087e668186106a
smasher164 <aindurti@gmail.com>
2021-11-15 15:38:26 -0500

    add pw-volume: basic interface to PipeWire volume controls

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "sway tools";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let supportedSystems = [
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-linux"
+    ]; in
+    flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        vscode-overlay = final: prev: {
+          vscode = prev.vscode.overrideAttrs (old: {
+            buildInputs = old.buildInputs or [ ] ++ [ final.makeWrapper ];
+            postFixup = old.postFixup or "" + ''
+              wrapProgram $out/bin/${pkgs.vscode.executableName} \
+                --add-flags "--enable-features=UseOzonePlatform --ozone-platform=wayland"
+            '';
+          });
+        };
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [ vscode-overlay ];
+        };
+        vscode-with-extensions = pkgs.vscode-with-extensions.override {
+          vscodeExtensions = [ pkgs.vscode-extensions.matklad.rust-analyzer ];
+        };
+      in
+      {
+        packages.pw-platform = pkgs.rustPlatform.buildRustPackage {
+          name = "pw-volume";
+          src = ./pw-volume/.;
+          cargoLock.lockFile = ./pw-volume/Cargo.lock;
+        };
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.cargo
+            pkgs.rustc
+            pkgs.clippy
+            pkgs.rustfmt
+            vscode-with-extensions
+          ];
+        };
+      });
+}
