cd8bda127332211b82f85940ee5224aac07de4a1
Akhil Indurti <aindurti@gmail.com>
2021-11-19 12:32:35 -0500

    vscodium fixes cursor scaling issues

--- flake.nix
+++ flake.nix
@@ -14,20 +14,20 @@
     flake-utils.lib.eachSystem supportedSystems (system:
       let
         vscode-overlay = final: prev: {
-          vscode = prev.vscode.overrideAttrs (old: {
+          vscodium = prev.vscodium.overrideAttrs (old: {
             buildInputs = old.buildInputs or [ ] ++ [ final.makeWrapper ];
             postFixup = old.postFixup or "" + ''
-              wrapProgram $out/bin/${pkgs.vscode.executableName} \
+              wrapProgram $out/bin/${pkgs.vscodium.executableName} \
                 --add-flags "--enable-features=UseOzonePlatform --ozone-platform=wayland"
             '';
           });
         };
         pkgs = import nixpkgs {
           inherit system;
-          config.allowUnfree = true;
           overlays = [ vscode-overlay ];
         };
         vscode-with-extensions = pkgs.vscode-with-extensions.override {
+          vscode = pkgs.vscodium;
           vscodeExtensions = [ pkgs.vscode-extensions.matklad.rust-analyzer ];
         };
       in
